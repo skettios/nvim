038bfde4ce47f7adda573cc9babd0f3dea4592f9
skettios <brandonjpascual@gmail.com>
2024-06-21 20:29:31 -0700

    changed stuff

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,3 +1,8 @@
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.wgsl",
+  callback = function() vim.bo.filetype = "wgsl" end,
+})
+
 if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
@@ -46,9 +51,9 @@ return {
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require("astronvim.plugins.configs.luasnip")(plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
+      local luasnip = require("luasnip")
       luasnip.filetype_extend("javascript", { "javascriptreact" })
     end,
   },
@@ -56,16 +61,16 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require("astronvim.plugins.configs.nvim-autopairs")(plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
+      local npairs = require("nvim-autopairs")
+      local Rule = require("nvim-autopairs.rule")
+      local cond = require("nvim-autopairs.conds")
       npairs.add_rules(
         {
           Rule("$", "$", { "tex", "latex" })
             -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
+            :with_pair(cond.not_after_regex("%%"))
             -- don't add a pair if  the previous character is xxx
             :with_pair(
               cond.not_before_regex("xxx", 3)
@@ -73,7 +78,7 @@ return {
             -- don't move right when repeat character
             :with_move(cond.none())
             -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
+            :with_del(cond.not_after_regex("xx"))
             -- disable adding a newline when you press <cr>
             :with_cr(cond.none()),
         },
